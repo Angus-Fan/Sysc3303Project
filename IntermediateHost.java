2cfdc4c0e2948ab95bbbd90463b6dec4f09e7949
Patrick990 <patrickaduwo@gmail.com>
2018-02-14 15:11:55 -0800

    Update IntermediateHost.java

--- IntermediateHost.java
+++ IntermediateHost.java
@@ -1,15 +1,14 @@
-//Patrick Aduwo
-//100962777
+//Group 8
 import java.net.*;
 import java.util.*;
-public class IntermediateHost{
+public class ErrorSimulator{
   
   DatagramSocket receiveSocket;//socket for receiving
   DatagramSocket SARSocket;//socket for sending and receiving
   DatagramPacket message;
   
   
-  IntermediateHost(){
+  ErrorSimulator(){
   }
   void run(){
     try {
@@ -20,41 +19,39 @@ public class IntermediateHost{
         
         System.out.println("\n====================================================================");
         System.out.println("Intermediate host: initially waiting to receive a request from the client");
-        byte[] b1 = new byte[20];
-        message = new DatagramPacket(b1,20);
+        byte[] b1 = new byte[516];
+        message = new DatagramPacket(b1,516);
         receiveSocket.receive(message);//wait to receive a message
-        System.out.println("message");
+        System.out.println("message received");
         
         String str = new String(message.getData());
         
-        print(str);//print the message as a string and bytes
-        
+        //print(str);
         //create a packet to send containing exactly what you received
         InetAddress address = InetAddress.getByName("127.0.0.1");
         DatagramPacket packet = new DatagramPacket(message.getData(), message.getLength(), address, 69);
-        System.out.println("Intermediate host: Sending a packet containing exactly what it received to the Server"); 
+        System.out.println("ErrorSimulator: Sending a packet containing exactly what it received to the Server"); 
+        Thread.sleep(3000);
         SARSocket.send(packet);//sends to the server
         
-        byte [] b2 = new byte[20];
-        DatagramPacket packet1 = new DatagramPacket(b2, 20);
+        byte [] b2 = new byte[516];
+        DatagramPacket packet1 = new DatagramPacket(b2, 516);
         
-        System.out.println("Intermediate host: Waiting for the server ");
+        System.out.println("ErrorSimulator: Waiting for the server ");
         SARSocket.receive(packet1);
         
         
-        System.out.println("Intermediate host: printing out information received from the Server");
+        System.out.println("ErrorSimulator: printing out information received from the Server");
         str = new String(packet1.getData());
         print(str);
         DatagramPacket packet2 = new DatagramPacket(packet1.getData(),packet1.getLength(),
                                                     message.getAddress(),message.getPort());
         DatagramSocket sendToClient = new DatagramSocket();
-        //String str2 = new String(packet1.getData());
-        //print(str2);
-        System.out.println("Intermediate host: sending packet to Client");
+        System.out.println("ErrorSimulator: sending packet to Client");
+        Thread.sleep(3000);
         sendToClient.send(packet2);
         
         System.out.println("Closing sockets");
-        
         sendToClient.close();
         receiveSocket.close();
         SARSocket.close();
@@ -63,8 +60,9 @@ public class IntermediateHost{
     } catch (Exception e){
       System.out.println("Error in run");
       //e.printStackTrace();
-      System.exit(1); 
+      System.exit(1);
     }
+    
   }
   void print(String str){
     //print out the information you're about to send
@@ -78,8 +76,8 @@ public class IntermediateHost{
   }
   
   public static void main(String [] args){
-    IntermediateHost intermediateHost = new IntermediateHost();
-    intermediateHost.run();
+    ErrorSimulator errorSimulator = new ErrorSimulator();
+    errorSimulator.run();
   }
   
 }
