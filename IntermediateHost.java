94f3658292e28115233a59297890909babacafb1
Patrick990 <patrickaduwo@gmail.com>
2018-03-23 20:16:17 -0700

    Update IntermediateHost.java

--- IntermediateHost.java
+++ IntermediateHost.java
@@ -9,12 +9,15 @@ public class ErrorSimulator extends Thread{
   DatagramPacket message = null;
   List<ErrorSimulator> errorSimulators;
   String name = "";
+  String numAnswer = "";
   
   DatagramSocket sendToClient = null;
   
-  int clientPort = -2;
-  int serverPort = -2;
+  int clientPort = 0;
+  int serverPort = 69;
   boolean loop = true;
+  //
+  Scanner scanner = null;
   
   //constructor for the main error Simulator
   ErrorSimulator(){
@@ -22,6 +25,7 @@ public class ErrorSimulator extends Thread{
       name = "main errorSimulator";
       errorSimulators = new ArrayList<ErrorSimulator>();
       receiveSocket = new DatagramSocket(23);
+      scanner = new Scanner(System.in);
     } catch (Exception e){
       System.out.println("Error in constructor");
     }
@@ -30,6 +34,7 @@ public class ErrorSimulator extends Thread{
   ErrorSimulator(DatagramPacket message1){
     this.message = message1;
     name = "not main errorSimulator";
+    scanner = new Scanner(System.in);
     //initialize the appropriate sockets
     try{
       sendToClient = new DatagramSocket();
@@ -44,8 +49,24 @@ public class ErrorSimulator extends Thread{
   public void run(){
     //repeat forever
     if(name.equals("main errorSimulator")){
-      while(loop){
+      System.out.println("What would you like to do:");
+      System.out.println("0 - normal operation, 1 - Invalid TFTP opcode on WRQ or RRQ, 2 - Invalid mode");
+      //numAnswer = scanner.nextLine().toLowerCase();
+      numAnswer = "2";
+      System.out.println(numAnswer);
+      
+      if(numAnswer.equals("0")){
+        System.out.println("Normal operation selected");
+      } else if(numAnswer.equals("1")){
+        System.out.println("ErrorSimulator: Invalid TFTP opcode on RRQ or WRQ");
+      } else if(numAnswer.equals("2")){
+        System.out.println("ErrorSimulator: Invalid mode selected");
         
+      } else {
+        System.out.println("Please try again");
+      }
+      
+      while(loop){
         System.out.println("\n====================================================================");
         System.out.println("main ErrorSimulator: initially waiting to receive a request from the client");
         byte[] b1 = new byte[516];
@@ -78,19 +99,15 @@ public class ErrorSimulator extends Thread{
         System.out.println("error in init address");
       }
       //send to part 69 (server)
-      boolean check = checkRequest(message.getData());
+      // boolean check = checkRequest(message.getData());
       
       DatagramPacket packet = null;
       //send to the server
-      if(check==true){
-        System.out.println("not main errorSimulator: initially sending request to server");
-        //it's a request, send to port 69 (main server)
-        packet = new DatagramPacket(message.getData(), message.getLength(), address, 69);
-      } else {
-        System.out.println("not main errorSimulator: sending packet (Data/ack) to server");
-        //send to the server thread handling this request
-        packet = new DatagramPacket(message.getData(), message.getLength(), address, serverPort);
-      }
+      
+      System.out.println("not main errorSimulator: sending packet to server");
+      //send to the server thread handling this request
+      packet = new DatagramPacket(message.getData(), message.getLength(), address, serverPort);
+      
       
       try{
         Thread.sleep(1000);
@@ -101,17 +118,19 @@ public class ErrorSimulator extends Thread{
       }
       
       //-----interaction----while loop --------------------------------------------------
-      while(true){
+      boolean eLoop = true;
+      
+      while(eLoop){
         //receive from server
         
         byte [] b2 = new byte[516];
         DatagramPacket receivedFromServer = new DatagramPacket(b2, 516);
         
-        System.out.println("ErrorSimulator: Waiting for the server ");
+        System.out.println("not main ErrorSimulator: Waiting for the server ");
         try{
           SARSocket.receive(receivedFromServer);
-          System.out.println("non main errorSimulator: printing...");
-
+          System.out.println("not main errorSimulator: printing...");
+          
         } catch(Exception e){
           System.out.println("Error in receiving a packet");
         }
@@ -126,7 +145,7 @@ public class ErrorSimulator extends Thread{
         print(str);
         DatagramPacket sendPacketToClient = new DatagramPacket(receivedFromServer.getData(),receivedFromServer.getLength(),
                                                                message.getAddress(),message.getPort());
-        loop = checkSize(message.getData());
+
         
         //socket to send back to the client
         //System.out.println("message port: "+message.getPort());
@@ -143,6 +162,20 @@ public class ErrorSimulator extends Thread{
         } catch(Exception e){
           System.out.println("Error in Thread.sleep/sending packet");
         }
+        
+        System.out.println(eLoop);
+        eLoop = checkSize(message.getData());
+        if(eLoop==false){
+          System.out.println("Size");
+          System.out.println("eLoop: "+eLoop);
+          break;
+        }
+        eLoop = checkOpCode(sendPacketToClient.getData());
+        if(eLoop==false){
+          System.out.println("not main ErrorSimulator: an errorPacket was sent");
+          break;
+        }
+        
         //receive from client
         System.out.println("errorSimulator: waiting for packet from client");
         
@@ -155,24 +188,37 @@ public class ErrorSimulator extends Thread{
         }
         
         str = new String(packetFromClient.getData());
-        print(str);
+        //print(str);
+        //---------------------
+
+        //-----------------------------
         
         System.out.println("errorSimulator: received packet from client");
+        eLoop = checkOpCode(packetFromClient.getData());
         System.out.println("errorSimulator: sending packet to server");
         DatagramPacket sendPacketToServer = new DatagramPacket(packetFromClient.getData(), packetFromClient.getLength(),
                                                                receivedFromServer.getAddress(), receivedFromServer.getPort());
         
         
         str = new String (packetFromClient.getData());
-        print(str);
+        //print(str);
         try{
-          
-          
           SARSocket.send(sendPacketToServer);
         } catch(Exception e){
           System.out.println("Error sending to server");
         }
-        //if(loop == false) break;
+        //---------------------
+        eLoop = checkSize(message.getData());
+        if(eLoop == false){
+          System.out.println("eLoop: "+eLoop);
+          break;
+        }
+        eLoop = checkOpCode(sendPacketToClient.getData());
+        if(eLoop == false){
+          System.out.println("not main ErrorSimulator: an errorPacket was sent");
+          break;
+        }
+        //-----------------------------
         
         
         System.out.println("===================================================================");
@@ -180,6 +226,11 @@ public class ErrorSimulator extends Thread{
     }
     shutdown();
     
+  }
+  
+  boolean checkOpCode(byte[] data){
+    if(data[0]==0 && data[1]==5) return false;
+    return true;
   }
   
   boolean checkRequest(byte[] data){
@@ -199,15 +250,20 @@ public class ErrorSimulator extends Thread{
   //shutdown the sockets
   private void shutdown(){
     System.out.println("Closing socket");
-    SARSocket.close();
-    sendToClient.close();
+    if(name.equals("not main ErrorSimulator")){
+      SARSocket.close();
+      sendToClient.close();
+    }
     
-    //wait for the other errorSimulators to finish
-    for(ErrorSimulator e: errorSimulators){
-      try{
-        e.join();
-      } catch(Exception ee){
-        System.out.println("Error in shuting down");
+    if(name.equals("main errorSimulator")){
+      receiveSocket.close();
+      //wait for the other errorSimulators to finish
+      for(ErrorSimulator e: errorSimulators){
+        try{
+          e.join();
+        } catch(Exception ee){
+          System.out.println("Error in shuting down");
+        }
       }
     }
     
@@ -225,12 +281,17 @@ public class ErrorSimulator extends Thread{
     System.out.println();
   }
   //check the size of the data being sent
-  boolean checkSize(byte[] data){
+  private boolean checkSize(byte[] data){
     if(data[0]==0 && data[1]==4) return true;//ACK message
     if(data[0]==0 && data[1]==5) return true;//error message
     
     for(int i =4;i<512;i++){
-      if(data[i]==0) return false;
+      
+      if(data[i]==0){
+        System.out.println(i+" "+data[i]);
+        return false;
+        
+      }
     }
     return true;
   }
